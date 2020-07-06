241e0b70503f9e2e2d9e6a6b4c0ed9bb2732ffb8
yongwoon <yonyon.japan@gmail.com>
2020-07-06 23:06:57 +0900

    load by dotenv in main.go

--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/yongwoon/echo-blog-api/config/initializer"
 )
 
 func main() {
+	initializer.InitDotenv()
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
