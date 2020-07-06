a39ed83776f9db74ce658005479d3275454bf9ff
yongwoon <yonyon.japan@gmail.com>
2020-07-06 23:32:22 +0900

    Set controller - GET api/v1/posts

--- route/route_v1.go
+++ route/route_v1.go
@@ -1,16 +1,15 @@
 package route
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo/v4"
 	"github.com/yongwoon/echo-blog-api/config"
+	v1controller "github.com/yongwoon/echo-blog-api/controller/v1"
 )
 
 func routeV1(e *echo.Echo) {
 	v1 := e.Group(config.APIVersion1)
+	posts := v1.Group("/posts")
 
-	v1.GET("", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	postHandler := v1controller.NewPost()
+	posts.GET("", postHandler.Index)
 }
