ca71817fead925ea6e5291086d12c3760f2cef7e
yongwoon kim <yonyon.japan@gmail.com>
2020-07-07 10:52:50 +0900

    Initialize db connection When app start

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/yongwoon/echo-blog-api/config/initializer"
+	"github.com/yongwoon/echo-blog-api/db"
 	"github.com/yongwoon/echo-blog-api/route"
 )
 
@@ -10,6 +11,8 @@ func main() {
 	initializer.InitDotenv()
 
 	e := echo.New()
+
+	db.Init()
 
 	route.Init(e)
 	e.Logger.Fatal(e.Start(":1323"))
