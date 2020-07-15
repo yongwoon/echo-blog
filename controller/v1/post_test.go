1eda5490ae860a8c95e840c27ca63fe8c90273e7
yongwoon <yonyon.japan@gmail.com>
2020-07-15 21:11:30 +0900

    TEST api/v1/posts

--- controller/v1/post_test.go
+++ controller/v1/post_test.go
@@ -1,22 +1,68 @@
 package v1
 
 import (
-    "testing"
-    . "github.com/smartystreets/goconvey/convey"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/labstack/echo/v4"
+	. "github.com/smartystreets/goconvey/convey"
+	"github.com/yongwoon/echo-blog/persistence"
+	"github.com/yongwoon/echo-blog/serializer"
+	"github.com/yongwoon/echo-blog/test"
 )
 
-func TestSpec(t *testing.T) {
+func TestPostIndexCaseGeneral(t *testing.T) {
+	Convey("GET api/v1/posts", t, func() {
+		Convey("post が存在する場合", func() {
+			e := echo.New()
+			test.Setup()
+			test.ImportFixture()
+			req := httptest.NewRequest(http.MethodGet, "/api/v1/posts", nil)
+			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+			rec := httptest.NewRecorder()
 
-	// Only pass t into top-level Convey calls
-	Convey("Given some integer with a starting value", t, func() {
-		x := 1
+			c := e.NewContext(req, rec)
+			c.SetPath("/api/v1/posts")
 
-		Convey("When the integer is incremented", func() {
-			x++
+			postPersistence := persistence.NewPostPersistence()
+			pc := NewPost(postPersistence)
+			pc.Index(c)
 
-			Convey("The value should be greater by one", func() {
-				So(x, ShouldEqual, 2)
-			})
+			var res serializer.PostListSerializer
+			test.ParseResponseBody(rec.Body, &res)
+
+			So(rec.Code, ShouldEqual, http.StatusOK)
+			So(res.PostsCount, ShouldEqual, 3)
+		})
+
+		Reset(func() {
+			test.Teardown()
+		})
+
+		Convey("post が存在しない場合", func() {
+			e := echo.New()
+			test.Setup()
+			req := httptest.NewRequest(http.MethodGet, "/api/v1/posts", nil)
+			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+			rec := httptest.NewRecorder()
+
+			c := e.NewContext(req, rec)
+			c.SetPath("/api/v1/posts")
+
+			postPersistence := persistence.NewPostPersistence()
+			pc := NewPost(postPersistence)
+			pc.Index(c)
+
+			var resultJSON = `{"posts":[],"postsCount":0}`
+
+			So(rec.Code, ShouldEqual, http.StatusOK)
+			So(strings.TrimSpace(rec.Body.String()), ShouldEqual, resultJSON)
+		})
+
+		Reset(func() {
+			test.Teardown()
 		})
 	})
 }
